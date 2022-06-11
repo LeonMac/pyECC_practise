42678a302343852de46ccd86aaf7b38b8276874f
LeonMac <liang.ma.sh@gmail.com>
2022-06-11 23:25:20 +0800

    update config

---
+++ config.py
@@ -0,0 +1,9 @@
+
+### Config for log show, control log.log
+LOG_SHOW_CFG = {
+'LOG_I' : False,    #INFO
+'LOG_D' : False,    #DEBUG
+'LOG_W' : True,     #WARNING
+'LOG_E' : True,     #ERROR
+'LOG_M' : True,     #MESSAGE, 
+}
