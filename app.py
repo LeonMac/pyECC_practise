b040c105b53345a6c127fd3eac9ee415205e92eb
LeonMac <liang.ma.sh@gmail.com>
2023-01-11 23:23:53 +0800

    some typo correction

--- app.py
+++ app.py
@@ -20,24 +20,24 @@ def KeyPair_Gen():
 
 def Encryption(fmt:str ='c1c3c2'):
     assert fmt in ('c1c2c3', 'c1c3c2'), f"incorrect output format :{fmt}"
-    x_hexstr = input("please input the receiver's public key x (hex with '0x' prefix):")
-    y_hexstr = input("please input the receiver's public key y (hex with '0x' prefix):")
+    x_hexstr = input("please paste the receiver's public key x (hex with '0x' prefix):")
+    y_hexstr = input("please paste the receiver's public key y (hex with '0x' prefix):")
     Pb = ECP((int(x_hexstr, 16), int(y_hexstr, 16)))
     #Pb.print_point()
 
-    M = input("please input the message you want to encrypted by using sm2:")
+    M = input("please input the message you want to encrypt by using sm2:")
 
     C  = cuv.SM2_Encryption(M, Pb, None, fmt, False)
 
-    print('Your encrypted message (hex):', C)
+    print('Your encrypted message (hex)--you can paste and send it to receiver now:', C)
 
     return C
 
 def Decryption(fmt:str ='c1c3c2'):
-    assert fmt in ('c1c2c3', 'c1c3c2'), f"incorrect output format :{fmt}"
+    assert fmt in ('c1c2c3', 'c1c3c2'), f"incorrect input format :{fmt}"
 
     C = input("please paste the encrypted message you want to do decryption here (hex):")
-    priv_hexstr = input("please input the your priv key (hex with '0x' prefix):")
+    priv_hexstr = input("please input your priv key (hex with '0x' prefix):")
     priv = int(priv_hexstr, 16)
     #print('Your priv key (never disclose it to anybody):', "{:064x}".format(priv))
     M_ = cuv.SM2_Decryption(C, priv, fmt, False)
@@ -50,7 +50,7 @@ if __name__ == '__main__':
     if len(sys.argv) != 3:
         print(f"usage: python {sys.argv} operation ( kg | en | de | full ), format( c1c2c3 | c1c3c2 )")
         print(f"kg = keygen, en = encryption, de = decryption, full = run all")
-        assert False, f'only accept one arguments. you input {len(sys.argv)} args!'
+        assert False, f'only accept 2 arguments. you input {len(sys.argv)} args!'
     
     opt = sys.argv[1] 
     assert opt in ('kg','en','de','full'), f"incorrect operation args input {opt}" 
